0537cc6e3645a059dbdb230490cfbc0461b4c2f4
Tim Shaw <tim.shaw@noblestudios.com>
2015-05-17 14:15:43 -0700

    Library searching is in

--- assets/scripts/app/controllers/Library.js
+++ assets/scripts/app/controllers/Library.js
@@ -1,12 +1,15 @@
 /**
  * Library controller.
  */
-App.controller( 'Library', [ '$scope', '$rootScope', '$element', 'spotifyApi', 'resources', 'spotifyConfig', 'dragAndDrop', 'spotifyUtility', function ( $scope, $rootScope, $element, spotifyApi, resources, spotifyConfig, dragAndDrop, spotifyUtility ) {
+App.controller( 'Library', [ '$scope', '$rootScope', '$element', 'spotifyApi', 'resources', 'spotifyConfig', 'dragAndDrop', 'spotifyUtility', 'debounce', function ( $scope, $rootScope, $element, spotifyApi, resources, spotifyConfig, dragAndDrop, spotifyUtility, debounce ) {
 
   /////////////////////////////////////////////////////////////////////////////
   // Init /////////////////////////////////////////////////////////////////////
   /////////////////////////////////////////////////////////////////////////////
 
+
+  // Search object for containing playlist/track queries
+  $scope.search = {};
 
   // Wait for resources to be loaded before going
   $scope.$on( 'resourcesReady', function () {
@@ -34,5 +37,20 @@ App.controller( 'Library', [ '$scope', '$rootScope', '$element', 'spotifyApi', '
   $scope.dragStop = function ( $event, ui, track ) {
     $rootScope.$broadcast( 'dragStop', track );
   };
+
+  $scope.searchLibrary = debounce( function () {
+    var query = $scope.search.library.toLowerCase();
+
+    if ( query ) {
+      var filtered = resources.library.filter( function ( track ) {
+        return track.track.name.toLowerCase().indexOf( query ) > -1 ||
+          track.track.artist_string.toLowerCase().indexOf( query ) > -1;
+      });
+
+      $scope.library = filtered;
+    } else {
+      $scope.library = resources.library;
+    }
+  }, 200 );
 
 }]);
--- assets/scripts/dist/application.js
+++ assets/scripts/dist/application.js
@@ -825,12 +825,15 @@ App.controller( 'Dropzone', [ '$scope', '$element', function ( $scope, $element 
 /**
  * Library controller.
  */
-App.controller( 'Library', [ '$scope', '$rootScope', '$element', 'spotifyApi', 'resources', 'spotifyConfig', 'dragAndDrop', 'spotifyUtility', function ( $scope, $rootScope, $element, spotifyApi, resources, spotifyConfig, dragAndDrop, spotifyUtility ) {
+App.controller( 'Library', [ '$scope', '$rootScope', '$element', 'spotifyApi', 'resources', 'spotifyConfig', 'dragAndDrop', 'spotifyUtility', 'debounce', function ( $scope, $rootScope, $element, spotifyApi, resources, spotifyConfig, dragAndDrop, spotifyUtility, debounce ) {
 
   /////////////////////////////////////////////////////////////////////////////
   // Init /////////////////////////////////////////////////////////////////////
   /////////////////////////////////////////////////////////////////////////////
 
+
+  // Search object for containing playlist/track queries
+  $scope.search = {};
 
   // Wait for resources to be loaded before going
   $scope.$on( 'resourcesReady', function () {
@@ -858,6 +861,21 @@ App.controller( 'Library', [ '$scope', '$rootScope', '$element', 'spotifyApi', '
   $scope.dragStop = function ( $event, ui, track ) {
     $rootScope.$broadcast( 'dragStop', track );
   };
+
+  $scope.searchLibrary = debounce( function () {
+    var query = $scope.search.library.toLowerCase();
+
+    if ( query ) {
+      var filtered = resources.library.filter( function ( track ) {
+        return track.track.name.toLowerCase().indexOf( query ) > -1 ||
+          track.track.artist_string.toLowerCase().indexOf( query ) > -1;
+      });
+
+      $scope.library = filtered;
+    } else {
+      $scope.library = resources.library;
+    }
+  }, 200 );
 
 }]);
 
